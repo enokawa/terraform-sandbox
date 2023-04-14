03201489c117687ef50642b40708d28bb86dd42c
enokawa <enokawa00@gmail.com>
2023-04-14 15:36:50 +0900

    terraform init

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "enokawa-terraform-sandbox-state"
+    key    = "terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.63.0"
+    }
+  }
+
+  required_version = ">= 1.4.5"
+}
