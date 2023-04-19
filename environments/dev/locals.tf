f42e688fc79532ea8ee6a3ee3c3964f3b1bd1038
enokawa <enokawa00@gmail.com>
2023-04-19 16:58:25 +0900

    Add provider settings for develop environment

--- environments/dev/locals.tf
+++ environments/dev/locals.tf
@@ -2,31 +2,3 @@ locals {
   aws_vpc_cidr_block = "10.0.0.0/16"
   prefix             = "sandbox"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "enokawa-terraform-sandbox-state"
-    key    = "aws/dev/terraform.tfstate"
-    region = "ap-northeast-1"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.63.0"
-    }
-  }
-
-  required_version = ">= 1.4.5"
-}
-
-provider "aws" {
-  region = "ap-northeast-1"
-
-  default_tags {
-    tags = {
-      ProjectName = "sandbox"
-      CreatedBy   = "Terraform"
-    }
-  }
-}
---
+++ environments/dev/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  backend "s3" {
+    bucket = "enokawa-terraform-sandbox-state"
+    key    = "aws/dev/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.63.0"
+    }
+  }
+
+  required_version = ">= 1.4.5"
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+
+  default_tags {
+    tags = {
+      ProjectName = "sandbox"
+      CreatedBy   = "Terraform"
+    }
+  }
+}
