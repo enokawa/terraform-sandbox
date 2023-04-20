d7a2d461de8a88fe544206c0e2f053b518d6a2b7
enokawa <enokawa00@gmail.com>
2023-04-20 10:42:45 +0900

    Add public subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,3 +16,14 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.env}-${var.prefix}-igw"
   }
 }
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.aws_subnet_cidr_block_public_1
+  availability_zone       = var.aws_subnet_az1
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.env}-${var.prefix}-public-1"
+  }
+}
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,6 @@
 variable "aws_vpc_cidr_block" {}
+variable "aws_subnet_cidr_block_public_1" {}
+variable "aws_subnet_az1" {}
+variable "aws_subnet_az2" {}
 variable "env" {}
 variable "prefix" {}
