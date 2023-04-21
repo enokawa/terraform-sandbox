4d80fcc2f7716d6ab061f12349643a96d0138878
enokawa <enokawa00@gmail.com>
2023-04-21 10:24:23 +0900

    Add public route table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -82,3 +82,16 @@ resource "aws_subnet" "private_2" {
     Name = "${var.env}-${var.prefix}-private-2"
   }
 }
+
+resource "aws_route_table" "public_1" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.env}-${var.prefix}-public-1"
+  }
+}
